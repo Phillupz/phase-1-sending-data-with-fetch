7b2a2d43725f1054a3a934ff74a184f5164cba89
Phillupz <phillup.zukowski@gmail.com>
2022-03-23 19:33:03 -0400

    commit tree 42d2caf21880b4b13d6a4a4caf1519e7c8234cff

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email) {
+const formData = {
+    name: name,
+    email: email,
+    };
+    
+const configObject = {
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+};
+return fetch("http://localhost:3000/users", configObject)
+.then((response) => response.json())
+.then((userData) => document.body.innerHTML = userData.id)
+.catch.replyWithError( {
+    message: message,
+    code: '401',
+  } )
+}
